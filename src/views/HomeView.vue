179493e0da85312b34abcdb5885f2fd5bbb13729
Oseias Pereira <oseiaspereira88@gmail.com>
2022-07-05 15:38:26 -0300

    Add router.js and refact components and layout

---
+++ src/views/HomeView.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="home">
+    <Section1/>
+    <Section2/>
+    <Section3/>
+    <Section4/>
+    <Section5/>
+    <Section6/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Section1 from '@/components/home/Section1.vue'
+import Section2 from '@/components/home/Section2.vue'
+import Section3 from '@/components/home/Section3.vue'
+import Section4 from '@/components/home/Section4.vue'
+import Section5 from '@/components/home/Section5.vue'
+import Section6 from '@/components/home/Section6.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    Section1,
+    Section2,
+    Section3,
+    Section4,
+    Section5,
+    Section6,
+  }
+}
+</script>
