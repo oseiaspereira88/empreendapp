bc4367873134de0d78d420735454a6144e78cf75
Oseias Pereira <oseiaspereira88@gmail.com>
2022-07-06 04:49:53 -0300

    Torna responsiva a Section1

---
+++ src/components/home/Section1Mobile.vue
@@ -0,0 +1,51 @@
+<template>
+  <v-container
+      style="max-width: 100vw; padding: 0 !important; background: linear-gradient(313deg, rgb(62,21,245) 25%, rgb(10,11,44) 100%);">
+
+    <div class="text-center pt-6" style="padding-left: 10vw; padding-right: 10vw;">
+      <lottie-animation id="animation1"
+                        class="mt-10"
+                        ref="anim"
+                        :animationData="require('@/assets/anim/programming-computer.json')"
+                        :loop="true"
+                        :autoPlay="true"
+                        :speed="10"/>
+      <h1 class="white--text" style="font-size: 32px; text-align: left;">
+        Nossa Software House
+      </h1>
+      <p class="white--text mb-8" style="font-size: 18px; text-align: justify;">
+        Pode usar toda a criatividade do mundo, só para tirar sua ideia do papel
+      </p>
+      <div style="width: 100%;">
+        <v-btn dark class="pt-1" rounded color="rgb(22,8,49)" style="width: 100%;">Solicite Orçamento</v-btn>
+      </div>
+      <div class="py-16"><br><br></div>
+    </div>
+  </v-container>
+</template>
+
+<script>
+
+import LottieAnimation from 'lottie-web-vue'
+
+export default {
+  name: 'Section1Mobile',
+  components: {
+    LottieAnimation
+  },
+  mounted() {
+    this.$refs.anim.play()
+  },
+  methods: {
+    loopComplete() {
+      console.log('loopComplete')
+    },
+    complete() {
+      console.log('complete')
+    },
+    enterFrame() {
+      console.log('enterFrame')
+    },
+  }
+}
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,18 @@
 <template>
-  <div class="home">
+  <div class="home" v-if="isMobile">
+    <Section1Mobile/>
+    <v-divider/>
+    <Section2/>
+    <v-divider/>
+    <Section3/>
+    <v-divider/>
+    <Section4/>
+    <v-divider/>
+    <Section5/>
+    <v-divider/>
+    <Section6/>
+  </div>
+  <div v-else>
     <Section1/>
     <v-divider/>
     <Section2/>
@@ -17,6 +30,7 @@
 <script>
 // @ is an alias to /src
 import Section1 from '@/components/home/Section1.vue'
+import Section1Mobile from '@/components/home/Section1Mobile.vue'
 import Section2 from '@/components/home/Section2.vue'
 import Section3 from '@/components/home/Section3.vue'
 import Section4 from '@/components/home/Section4.vue'
@@ -27,11 +41,27 @@ export default {
   name: 'HomeView',
   components: {
     Section1,
+    Section1Mobile,
     Section2,
     Section3,
     Section4,
     Section5,
     Section6,
+  },
+  data: () => {
+    return {
+      isMobile: true,
+      showNav: false
+    }
+  },
+  methods: {
+    handleView() {
+      this.isMobile = window.innerWidth <= 880;
+    },
+  },
+  created() {
+    this.handleView();
+    window.addEventListener('resize', this.handleView);
   },
 }
 </script>
